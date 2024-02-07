8cf7cc9838a112143037cd3ef4803d12fba1424e
thomas-rocha <trocha@ucsd.edu>
2024-02-06 19:14:37 -0800

    Bug Fixes
    Fixed bugs regarding the reverse() and reverseInPlace() methods in the ArrayExamples file.

--- ArrayTests.java
+++ ArrayTests.java
@@ -1,4 +1,7 @@
 import static org.junit.Assert.*;
+
+import java.sql.Array;
+
 import org.junit.*;
 
 public class ArrayTests {
@@ -14,5 +17,26 @@ public class ArrayTests {
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversedWithElems(){
+    int[] input1 = {1,2,3};
+    assertArrayEquals(new int[]{3,2,1}, ArrayExamples.reversed(input1));
+  }
+  
+  @Test
+  public void testReverseInPlace2(){
+    int[] input1 = {1,2,3};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{3,2,1}, input1);
+  }
+  @Test
+  public void testReverseInPlaceElems(){
+    int[] input1 = {1,2,3};
+    ArrayExamples.reverseInPlace(input1);
+    assertEquals(3, input1[0]);
+    assertEquals(2, input1[1]);
+    assertEquals(1, input1[2]);
   }
 }
